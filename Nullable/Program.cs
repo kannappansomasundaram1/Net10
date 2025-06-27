fec51cd8542a11779a9f6dd3a2bfdc0b30d05fb5
kannappan.somasundaram <kannappan.somasundaram@thetrainline.com>
2025-06-27 06:21:38 +0100

    extensions and field

--- Nullable/Program.cs
+++ Nullable/Program.cs
@@ -9,9 +9,6 @@ var customer1 = new Customer();
 customer1?.Name = "John Doe";
 Console.WriteLine(customer1?.Name?? "No customer found");
 
-Console.WriteLine(customer?.Name); 
-
-
 
 public class Customer
 {
